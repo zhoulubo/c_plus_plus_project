31815f78722f44eb87bbbd984c8fada4ccae0a7c
zhoulubo <zhoulubo@honhesns.com>
2019-07-26 15:34:38 +0800

    ok

---
+++ C_plus_plus_project/c_plus_plus_03.cpp
@@ -0,0 +1,23 @@
+#include "stdafx.h"
+
+
+//#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <vadefs.h>
+#include <stdarg.h>
+
+using namespace std;
+
+// 构造函数带对象参数的构造方式
+
+
+
+
+
+
+void main23(){
+	
+	getchar();
+}
